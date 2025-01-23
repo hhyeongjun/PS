d68ff24358a82dfdf52c12b8d84163e98200097a
Hyeongjun Lee <123728524+hhyeongjun@users.noreply.github.com>
2025-01-23 23:35:22 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 448 ms, Memory: 21404 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        // 상단 삼각형 (N개 행)
+        for (int i = 1; i <= N; i++) {
+            // 공백 출력
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 하단 삼각형 (N-1개 행)
+        for (int i = N - 1; i >= 1; i--) {
+            // 공백 출력
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            // 별 출력
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
